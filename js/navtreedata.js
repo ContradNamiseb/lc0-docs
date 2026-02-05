a33e6e70af25dc71203e7cb6f9407de11c8e57ce
Contrad Namiseb <cenamiseb@gmail.com>
2026-02-05 23:21:41 +0200

    Fix navigation: correct initMenu relPath for sub-pages

---
+++ js/navtreedata.js
@@ -0,0 +1,118 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Leela Chess Zero", "index.html", [
+    [ "Todo List", "pages/todo.html", null ],
+    [ "Namespaces", "pages/namespaces.html", [
+      [ "Namespace List", "pages/namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "pages/namespacemembers.html", [
+        [ "All", "pages/namespacemembers.html", "namespacemembers_dup" ],
+        [ "Functions", "pages/namespacemembers_func.html", "namespacemembers_func" ],
+        [ "Variables", "pages/namespacemembers_vars.html", null ],
+        [ "Typedefs", "pages/namespacemembers_type.html", null ],
+        [ "Enumerations", "pages/namespacemembers_enum.html", null ],
+        [ "Enumerator", "pages/namespacemembers_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "pages/annotated.html", [
+      [ "Class List", "pages/annotated.html", "annotated_dup" ],
+      [ "Class Index", "pages/classes.html", null ],
+      [ "Class Hierarchy", "pages/hierarchy.html", "hierarchy" ],
+      [ "Class Members", "pages/functions.html", [
+        [ "All", "pages/functions.html", "functions_dup" ],
+        [ "Functions", "pages/functions_func.html", "functions_func" ],
+        [ "Variables", "pages/functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "pages/functions_type.html", null ],
+        [ "Enumerations", "pages/functions_enum.html", null ],
+        [ "Enumerator", "pages/functions_eval.html", null ],
+        [ "Related Symbols", "pages/functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "pages/files.html", [
+      [ "File List", "pages/files.html", "files_dup" ],
+      [ "File Members", "pages/globals.html", [
+        [ "All", "pages/globals.html", "globals_dup" ],
+        [ "Functions", "pages/globals_func.html", null ],
+        [ "Variables", "pages/globals_vars.html", "globals_vars" ],
+        [ "Typedefs", "pages/globals_type.html", null ],
+        [ "Macros", "pages/globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"pages/MetaCommand_8h.html",
+"classOpenCL.html#a450667d8c14991c95ed891dbb838c31a",
+"classlczero_1_1Benchmark.html#a86427e0e46147320645eed50611df4cb",
+"classlczero_1_1ChessBoard_1_1Castlings.html#a063e7eaeb8e09e792560a0bfdfe74e05",
+"classlczero_1_1CudaNetworkComputation.html#a68be8566dc1d5baec280adfd456e6a1d",
+"classlczero_1_1Engine_1_1UciPonderForwarder.html#aeca40a759a588a79d211b246a8b943a2",
+"classlczero_1_1HashKeyedCacheLock.html#a8792949230ea8a649ad9318761223077",
+"pages/classlczero_1_1KingAttackInfo.html",
+"classlczero_1_1Move.html#a2bdbf385143f7ca8d780338aeff5c49aaa9f9e68ea1da0c58e0966cae68e83ae5",
+"classlczero_1_1OnednnNetworkComputation.html#a9898705ed8679dd2fb2088eaa5d76905",
+"classlczero_1_1OptionsParser.html#ad5b81d53023b32640c828bc7c327cdaf",
+"classlczero_1_1Position.html#a9f5f6a5c2faf1c8b7bf974a9dbd267b9",
+"classlczero_1_1SelfPlayGame.html#af6a3e9bd2671a3d9eb1f7dab8539465c",
+"classlczero_1_1SyclNetwork.html#a4d1f128c18044271ba8d06e7eb585fda",
+"pages/classlczero_1_1UciResponder.html",
+"classlczero_1_1classic_1_1BaseSearchParams.html#a38994bc329910803e5cbd67abbcfc47c",
+"classlczero_1_1classic_1_1BaseSearchParams.html#ae87ca9b3fa27be781b4b1a7698054b39",
+"classlczero_1_1classic_1_1Node.html#a83975e2bea792bcf8ceea611f9c2e606",
+"classlczero_1_1classic_1_1Search.html#ac2da29a307cd064614b58dd05674ea64",
+"classlczero_1_1cudnn__backend_1_1AttentionBody.html#a1593722c8a0984c47aca3ec0dffc9f22",
+"classlczero_1_1cudnn__backend_1_1EncoderBlock.html#a2cb98a26ddb1178aad13dce820abd004",
+"classlczero_1_1cudnn__backend_1_1ValueHead.html#a4e5cadbce3a3b750b733dfaa2463b60a",
+"classlczero_1_1dag__classic_1_1Node.html#a1d8f491ff968a14674d4b5da5d8ea5b2",
+"classlczero_1_1dag__classic_1_1Search.html#a2cf775dfadbc9c6c8efde010b298387d",
+"classlczero_1_1dag__classic_1_1VisitedNode__Iterator.html#ae726b5f0d5b5be92bcb6e99939943013",
+"classlczero_1_1dx__backend_1_1DxNetwork.html#aa04c4cf3dc4e12d381a14898e7ce7e26",
+"classlczero_1_1metal__backend_1_1MetalNetworkComputation.html#a3e1c8ed99b0de06181a05d8e41e9469f",
+"classlczero_1_1onnx_1_1OnnxComputation.html#ab6f92a21f6b62eb5b2cb66dd2782dce9",
+"classlczero_1_1sycldnn__backend_1_1AttentionBody.html#af1762a929e55d7768973fe08ce810287",
+"classlczero_1_1sycldnn__backend_1_1FCLayer.html#a6fa896dd187abfb27d9450c2e9a9d266",
+"common__kernels_8dp_8cpp.html#a72b1a8d88a0840aa89038922a8dd9323",
+"pages/dir_6622a30ede60f61f2e6ff62aec52b54e.html",
+"pages/functions_type.html",
+"pages/metal__common_8h.html",
+"namespacelczero.html#ab8f8d7eebdf32c9d8aa6098b2b0d0f30a8ba8ac037709b266f6ec856ea77e4489",
+"namespacelczero_1_1sycldnn__backend.html#a44a89510992a10b158bbf1e82278d20a",
+"network__demux_8cc.html#acd028c5d0b59eb5b570501146ab3882a",
+"optionsparser__test_8cc.html#a3c04138a5bfe5d72780bb7e82a18e627",
+"smooth_8cc.html#af68f005c94c5ab2798953d2df356ab5b",
+"structlczero_1_1BestMoveInfo.html#a99cea8532cb4b31c9463b00c1a2c010e",
+"structlczero_1_1HashKeyedCache_1_1Entry.html#a85bbaf1b7cd93b5cb317310a069d543b",
+"pages/structlczero_1_1PieceType.html",
+"structlczero_1_1V6TrainingData.html#a9faceae8e4b48a7b1721ec8ef763b56c",
+"structlczero_1_1cudnn__backend_1_1InputsOutputs.html#a7306070b1b58feb189bc380e0071e7a1",
+"structlczero_1_1metal__backend_1_1InputsOutputs.html#adc6a6df27146eb5639de3ea554c585f1",
+"pages/winograd__convolution3_8h_source.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
